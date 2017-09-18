9bf6b958088748c7d3ac0164d4d3ec227f44e0b7
OlgaVasy <ovasilyeva22@gmail.com>
2017-09-18 16:36:20 -0700

    initial commit

---
+++ src/com/company/Numbers.java
@@ -0,0 +1,18 @@
+// Ex. 2.14: Numbers.java
+// Displaying the numbers 1 to 4 on the same line
+package com.company;
+
+public class Numbers {
+
+    public static void main(String[] args) {
+
+       System.out.println("1 2 3 4");
+
+       System.out.print("1 ");
+       System.out.print("2 ");
+       System.out.print("3 ");
+       System.out.print("4");
+
+       System.out.printf("%n%s%s%s%s","1 ","2 ","3 ","4 ");
+    }
+}
